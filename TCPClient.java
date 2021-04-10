91b53e57e669be8a2a59946efb395e6e6e43e173
Zack Hollander <zdhollander@gmail.com>
2021-04-10 10:38:37 -0400

    Created Send and Received methods

--- TCPClient.java
+++ TCPClient.java
@@ -3,30 +3,26 @@ import java.net.*;
 
 class TCPClient {
 
+    public static void receive(BufferedReader inFromServer) throws Exception{
+        String fromServer = inFromServer.readLine();
+        System.out.println(fromServer = "\n");
+    }
+
+    public static void send(BufferedReader inFromUser, DataOutputStream outToServer) throws Exception{
+        String toServer = inFromUser.readLine();
+        outToServer.writeBytes(toServer + "\n");
+    }
+
     public static void main(String argv[]) throws Exception {
 
-        String sentence;
-        String modifiedSentence;
-
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-
         Socket clientSocket = new Socket("localhost", 5000);
-
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         while (true) {
-
-            System.out.println("Please enter the text that you want to send to the server \n");
-
-            sentence = inFromUser.readLine();
-
-            outToServer.writeBytes(sentence + "\n");
-
-            modifiedSentence = inFromServer.readLine();
-
-            System.out.println("RECEIVED FROM SERVER: " + modifiedSentence);
+            send(inFromUser, outToServer);
+            receive(inFromServer);
         }
 
     }
