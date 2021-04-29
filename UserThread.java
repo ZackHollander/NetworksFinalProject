eeb75a702c3a5616666e89e3b7a5394f5abe0e88
Zack Hollander <zdhollander@gmail.com>
2021-04-29 14:06:06 -0400

    Added class comments.

--- UserThread.java
+++ UserThread.java
@@ -1,6 +1,21 @@
+/*
+* Auction Application 
+* 2020
+*
+* This application is made by Zack Hollander for his Networks 352 class at Dickinson College. 
+* Please feel free to edit and play around with the code. Use this code to learn about TCP 
+* applications. The application simulates a chat with an auction functionality. ENJOY!
+*/
+
 import java.io.*;
 import java.net.*;
 
+/*
+* Takes the specific user socket created by the AuctionServer and waits for the users input.
+* UserThread will then parse the input and decided the specific action to take place next.
+* This includes broadcasting a messge, adding an auction item, bidding on an acution item, or
+* lisitng all items up for auction.  
+*/
 public class UserThread extends Thread {
     private Socket socket;
     private AuctionServer server;
